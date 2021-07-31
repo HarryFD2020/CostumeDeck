d0fe133b4dedc75c57efae81f54158a748d2a4b9
HarryFD <haotian0895@126.com>
2021-07-31 22:35:06 +0800

    通用回调代码建立

--- DeckProj/Assets/Scripts/Bamboom/CoroutineMgr.cs
+++ DeckProj/Assets/Scripts/Bamboom/CoroutineMgr.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,25 +7,70 @@ namespace Bamboom.Framework
 {
     public class CoroutineMgr : MonoBehaviour
     {
-        public static CoroutineMgr Instance;
+        public static CoroutineMgr Instance { get {
+                if (Instance == null)
+                {
+                    var ins = new GameObject("CoroutineMgr");
+                    Instance = ins.AddComponent<CoroutineMgr>();    
+                }
+                return Instance;
+            } 
+            set {
+                if (Instance != null)
+                {
+                    Destroy(Instance.gameObject);
+                }
+                Instance = value;
+            } }
 
         public static Coroutine StartCoroutine(IEnumerator enumerator, bool doNotDestroy = false)
         {
-            if (Instance == null)
-            {
-                var ins = new GameObject("CoroutineMgr");
-                Instance = ins.AddComponent<CoroutineMgr>();
-
-                if (doNotDestroy) { DontDestroyOnLoad(Instance.gameObject); }
-            }
+            if (doNotDestroy) { DontDestroyOnLoad(Instance); }
 
             return (Instance as MonoBehaviour).StartCoroutine(enumerator);
         }
 
+        public static new void StopCoroutine(IEnumerator enumerator)
+        {
+            (Instance as MonoBehaviour).StopCoroutine(enumerator);
+        }
+
         private void OnDestroy()
         {
+            StopAllCoroutines();
             if (Instance == this) { Instance = null; }
         }
+    }
+
+    public class TimeHold
+    {
+        public static Coroutine DoActionAfterSec(Action action, float duration, bool inRealTime = false)
+        {
+            return CoroutineMgr.StartCoroutine(ActionDelaySec(action, duration, inRealTime));
+        }
+
+        public static Coroutine DoActionAfterFrame(Action action, int num = 1)
+        {
+            return CoroutineMgr.StartCoroutine(ActionDelayFrame(action, num));
+        }
+
+        private static IEnumerator ActionDelaySec(Action action, float duration, bool inRealTime)
+        {
+            if (inRealTime) { yield return new WaitForSecondsRealtime(duration); }
+            else { yield return new WaitForSeconds(duration); }
+            action.Invoke();
+        }
+
+        private static IEnumerator ActionDelayFrame(Action action, int num)
+        {
+            num = num > 0 ? num : 1;
+            for (int i = 0; i < num; i++)
+            {
+                yield return null;
+            }
+            action.Invoke();
+        }
+
     }
 }
 
