5a9eed6d9c1ab690c8d5260d4de0355cfc646ad1
HarryFD <haotian0895@126.com>
2021-07-31 10:42:54 +0800

    Unity Setup

---
+++ DeckProj/Assets/Scripts/Bamboom/CoroutineMgr.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Bamboom.Framework
+{
+    public class CoroutineMgr : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
+
+
